a2976896fada68932521c4603d7fd55004510517
Aadeesh11 <asoswal@gmail.com>
2024-02-23 19:42:39 +0530

    role and community done

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import Sequelize from "sequelize";
 import DataTypes from "sequelize";
 import { Snowflake } from "@theinternetfolks/snowflake";
-import { get } from "http";
 
 const port = 3000 || process.env.PORT;
 
@@ -58,7 +57,7 @@ const Community = sequelize.define("Community", {
   },
 });
 
-const role = sequelize.define("role", {
+const Role = sequelize.define("role", {
   id: {
     type: DataTypes.STRING,
     primaryKey: true,
@@ -69,7 +68,7 @@ const role = sequelize.define("role", {
   },
 });
 
-const member = sequelize.define("member", {
+const Member = sequelize.define("member", {
   id: {
     type: DataTypes.STRING,
     primaryKey: true,
@@ -97,7 +96,7 @@ const member = sequelize.define("member", {
     allowNull: false,
     references: {
       foriegnKey: true,
-      model: role,
+      model: Role,
       key: "id",
     },
   },
@@ -106,9 +105,7 @@ const member = sequelize.define("member", {
 function getsuccessStruct(data) {
   return {
     status: true,
-    content: {
-      data,
-    },
+    content: data,
   };
 }
 
@@ -130,21 +127,146 @@ app.post("/v1/role", (req, res) => {
       })
     );
   } else {
-    role.create({ id: Snowflake.generate(), name }).then((role) => {
-      res.json(getsuccessStruct(role));
+    Role.create({ id: Snowflake.generate(), name }).then((role) => {
+      res.json(getsuccessStruct({ data: role }));
     });
   }
 });
 
 app.get("/v1/role", (req, res) => {
-  role.findAndCountAll({ limit: 10, offset: page * 10 }).then((roles) => {
+  let { page } = req.query;
+  if (page === undefined || page < 0) {
+    page = 0;
+  }
+  Role.findAndCountAll({ limit: 10, offset: page * 10, where: {} }).then(
+    (roles) => {
+      res.json(
+        getsuccessStruct({
+          meta: {
+            page: page + 1,
+            total: roles.count,
+            pages: parseInt(roles.count / 10) + 1,
+          },
+          data: roles.rows,
+        })
+      );
+    }
+  );
+});
+
+app.post("/v1/community", (req, res) => {
+  const { name } = req.body;
+  if (name === undefined || name.length <= 2) {
+    res.json(
+      getErrorStruct({
+        param: name,
+        message: "Name must be at least 2 characters long",
+        code: "INVALID_INPUT",
+      })
+    );
+  } else {
+    const user_id = req.decoded.id;
+    Community.create({
+      id: Snowflake.generate(),
+      name,
+      slug: name.toLowerCase(),
+      owner: user_id,
+    }).then((community) => {
+      Member.create({
+        id: Snowflake.generate(),
+        user: user_id,
+        community: community.id,
+        role: Role.findOne({ where: { name: "Community Admin" } }).id,
+      });
+      res.json(getsuccessStruct({ data: community }));
+    });
+  }
+});
+
+app.get("/v1/community", (req, res) => {
+  let { page } = req.query;
+  if (page === undefined || page < 0) {
+    page = 0;
+  }
+  Community.findAndCountAll({
+    limit: 10,
+    offset: page * 10,
+    where: { owner: req.decoded.id },
+    include: [
+      {
+        model: User,
+        required: true,
+        attributes: ["id", "name"],
+        as: "owner",
+      },
+    ],
+  }).then((communities) => {
     res.json(
       getsuccessStruct({
         meta: {
-          page: 1,
-          total: 10,
-          limit: 10,
+          page: page + 1,
+          total: communities.count,
+          pages: parseInt(communities.count / 10) + 1,
         },
+        data: communities.rows,
+      })
+    );
+  });
+});
+
+app.get("/v1/community/:id/members", (req, res) => {
+  let { page } = req.query;
+  if (page === undefined || page < 0) {
+    page = 0;
+  }
+  Member.findAndCountAll({
+    limit: 10,
+    offset: page * 10,
+    where: { community: req.params.id },
+    include: [
+      {
+        model: User,
+        required: true,
+        attributes: ["id", "name"],
+        as: "user",
+      },
+      {
+        model: Role,
+        required: true,
+        attributes: ["id", "name"],
+        as: "role",
+      },
+    ],
+  }).then((members) => {
+    res.json(
+      getsuccessStruct({
+        meta: {
+          page: page + 1,
+          total: members.count,
+          pages: parseInt(members.count / 10) + 1,
+        },
+        data: members.rows,
+      })
+    );
+  });
+});
+
+app.get("/v1/community/me/owner", (req, res) => {
+  let { page } = req.query;
+  if (page === undefined || page < 0) {
+    page = 0;
+  }
+  Community.findAndCountAll({
+    where: { owner: req.decoded.id },
+  }).then((communities) => {
+    res.json(
+      getsuccessStruct({
+        meta: {
+          page: page + 1,
+          total: communities.count,
+          pages: parseInt(communities.count / 10) + 1,
+        },
+        data: communities.rows,
       })
     );
   });
